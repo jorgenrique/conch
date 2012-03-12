abd6903d97518c7d1bd35a5ab11633e93ac34de6
Jorge <yo@jorgenrique.com>
2012-03-12 00:58:57 -0400

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
   
+  def index
+      @user = User.find(params[:all])
+      @title = 'Users'
+  end
+  
   def show
       @user = User.find(params[:id])
+      @title = @user.name
   end
     
   def new
+    @user = User.new
     @title = 'Sign Up'
   end
-
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Thank you for signing up!"
+      redirect_to @user
+    else
+      @title = 'Sign Up'
+      render 'new'
+    end
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120307042214) do
+ActiveRecord::Schema.define(:version => 20120311213357) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(:version => 20120307042214) do
     t.integer  "ref_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
