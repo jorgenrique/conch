e3791878d179bbd1cf6397f996b52347e3f36205
Jorge <yo@jorgenrique.com>
2012-03-15 01:54:46 -0400

    sign up/in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:session][:email],
                                params[:session][:password])
       if user.nil?
-        flash.now[:error] = "Invalid email/password combination."
+        flash.now[:error] = "We're sorry, invalid email/password combination."
               @title = "Sign in"
               render 'new'
       else
         sign_in user
-        redirect_to user
+        redirect_to root_path
       end
   end
   
