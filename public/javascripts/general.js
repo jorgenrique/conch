0c82d5b455f249629b277e61aa08c25ccfcdf5a6
Jorge <yo@jorgenrique.com>
2012-03-27 00:00:16 -0400

    header + logo

--- public/javascripts/general.js
+++ public/javascripts/general.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   	
+	$('#nav_menu').children('ul').children('li').first().addClass('red');
+
 	$('#nav_menu').children('ul').children('li').click(function(){
 		$('#nav_menu').children('ul').children('li').removeClass('red');
 		$(this).addClass('red');
